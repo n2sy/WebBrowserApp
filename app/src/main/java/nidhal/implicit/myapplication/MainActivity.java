4a1713606a4d94cd1030f1b00367ce98639e3781
nidhal <nidhal.jelassi@fsegt.utm.tn>
2020-11-26 18:19:11 +0100

    Initial commit

---
+++ app/src/main/java/nidhal/implicit/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package nidhal.implicit.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent i = getIntent();
+        Uri uri = i.getData();
+        if (uri != null) {
+            String uri_string = getString(R.string.uri_label, uri.toString());
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uri_string);
+        }
+    }
+}
